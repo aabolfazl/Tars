82ab8110d567b983b34c1b8e1ad04b332b3a48f3
aabolfazl <aabolfazlit@gmail.com>
2024-01-28 12:02:48 +0300

    Add message and directory reader

--- src/LogReader.py
+++ src/LogReader.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import datetime
+import os
 
 from FileHandler import FileHandler
 from LogParser import LogParser
@@ -7,45 +9,91 @@ from LogParser import LogParser
 class LogReader:
     def __init__(self, args):
         print(args)
-        self._args = args
         self._tags = set()
-        self._output_file = args.output_file
-        self._input_file = args.input_file
+        self._messages = set()
+        self._log_file = args.input
+        self._line_count = 0
+        self._show_exception = args.exception
+        self._parser = LogParser()
+
+        if args.output:
+            self._output = args.output
+        else:
+            self._output = os.path.join(self._log_file, f"tmp.txt")
+
         self.set_tags(args)
+        self.set_messages(args)
 
     def set_tags(self, args):
         if args.tags:
             for tag in args.tags:
                 self._tags.add(tag)
 
-    def start(self):
-        parser = LogParser()
-        file_reader = FileHandler(self._input_file, 'r')
-        file_reader.open_file()
-        line_count = 0
+    def set_messages(self, args):
+        if args.messages:
+            for message in args.messages:
+                self._messages.add(message)
 
-        if self._output_file:
-            file_writer = FileHandler(self._output_file, 'w')
-            file_writer.open_file()
+    def start(self):
+        is_directory = os.path.isdir(self._log_file)
+
+        output_handler = FileHandler(self._output, 'w')
+        output_handler.open_file()
+
+        if is_directory:
+            all_files = os.listdir(self._log_file)
+            sorted_files = sorted(all_files)
+
+            for filename in sorted_files:
+                if filename.startswith("tmp"):
+                    continue
+
+                file_path = os.path.join(self._log_file, filename)
+                if os.path.isfile(file_path):
+                    output_handler.write_line("\n\nstart of file: " + file_path)
+                    self.start_single(file_path, output_handler)
         else:
-            file_writer = None
+            self.start_single(self._log_file, output_handler)
+
+        if output_handler:
+            output_handler.close_file()
+            current_time = datetime.datetime.now().strftime("%d_%m_%Y_%H:%M:%S")
+            new_file_path = os.path.join(os.path.dirname(self._output), f"logs_{current_time}.txt")
+            os.rename(self._output, new_file_path)
+
+        print(f"Done, {self._line_count} Read")
+
+    def start_single(self, file_path, output_handler):
+        file_reader = FileHandler(file_path, 'r')
+        file_reader.open_file()
 
         while True:
             line = file_reader.read_line()
             if line is None:
                 break
 
-            line_count += 1
-            parsed_line = parser.parse(line)
+            if not self._tags and not self._messages:
+                break
 
-            if parsed_line is not None and parsed_line["tag"] in self._tags:
-                if file_writer:
-                    file_writer.write_line(line)
-                else:
-                    print(line)
+            self._line_count += 1
+            parsed_line = self._parser.parse(line)
+
+            if parsed_line is not None:
+                tag = parsed_line['tag']
+                message = parsed_line['message']
+
+                should_write = tag in self._tags or self.contain_messages(message) or self.show_exception(tag, message)
+
+                if should_write:
+                    output_handler.write_line(line)
 
         file_reader.close_file()
-        if file_writer:
-            file_writer.close_file()
 
-        print(f"Done, {line_count} Read")
+    def contain_messages(self, read) -> bool:
+        for message in self._messages:
+            if message in read:
+                return True
+        return False
+
+    def show_exception(self, tag, message) -> bool:
+        return self._show_exception and tag == "E" or self._show_exception and "Exception" in message
--- src/Log_reader.py
+++ src/Log_reader.py
@@ -14,9 +14,11 @@ def main():
 
     parser = argparse.ArgumentParser(description='Process some files and parameters.')
 
-    parser.add_argument('input_file', type=str, help='The input file path')
-    parser.add_argument('-t', '--tags', nargs='+', help='List of tags')
-    parser.add_argument('-o', '--output_file', type=str, help='The output file path')
+    parser.add_argument('-i', '--input', type=str, help='The input file or directory path', required=True)
+    parser.add_argument('-t', '--tags', nargs='+', help='List of tags', required=False)
+    parser.add_argument('-m', '--messages', nargs='+', help='List of messages', required=False)
+    parser.add_argument('-o', '--output', type=str, help='The output file path', required=False)
+    parser.add_argument('-e', '--exception', type=str, help='Show exception', required=False, default=False)
 
     args = parser.parse_args()
     reader = LogReader(args)
